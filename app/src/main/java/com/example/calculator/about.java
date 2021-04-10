062d3fa7224112a8c219b3527e873f9c1f5e3c7f
Elopower1337 <kbalandin.99@bk.ru@>
2021-04-10 20:55:23 +0300

    calc222
    zadanie3

---
+++ app/src/main/java/com/example/calculator/about.java
@@ -0,0 +1,23 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class about extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public  boolean onSupportNavigateUp()
+    {
+        onBackPressed();
+        return true;
+    }
+}
